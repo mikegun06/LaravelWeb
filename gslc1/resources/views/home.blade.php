ae8e1d15c1287c41b3285de70673c38114e19a68
Michael <81206421+mikegun06@users.noreply.github.com>
2022-11-17 00:09:13 +0700

    home_change1

--- gslc1/resources/views/home.blade.php
+++ gslc1/resources/views/home.blade.php
@@ -1,5 +1,17 @@
 @extends('template')
 
 @section('container')
-<h1>GSLC Web Programming using Laravel 8</h1>
+<div class="sect-1 mb-3">
+    <h1>GSLC Web Programming using Laravel 8</h1>
+</div>
+<div class="sect-2 mb-3">
+    <h5>This is main body</h5>
+</div>
+<div class="sect-3 mb-3">
+    <p>
+        This is closing
+        <br>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate nostrum amet doloribus porro modi, facilis, incidunt qui eligendi, exercitationem nemo repellendus necessitatibus ab esse aperiam. Optio debitis impedit corrupti explicabo nesciunt odio quasi ea quae?
+    </p>
+</div>
 @endsection
